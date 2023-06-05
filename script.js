fc3395cd76122a6eb22a466c19dda1832b7b4b83
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-06-05 17:32:14 -0300

    Javascript responsive menu

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function copyMenu(){
 }
 
 copyMenu()
+
+const menuButton = document.querySelector('.trigger')
+const closeButton = document.querySelector('.t-close')
+const addclass = document.querySelector('.site')
+menuButton.addEventListener('click', function(){
+    addclass.classList.toggle('showmenu')
+})
+
+
+closeButton.addEventListener('click', function(){
+    addclass.classList.remove('showmenu')
+})
+
+const submenu = document.querySelectorAll('.has-child .icon-small')
+submenu.forEach((menu) => menu.addEventListener('click', toggle))
+
+function toggle(e){
+    e.preventDefault()
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null)
+    if(this.closest('.has-child').classList != 'expand'){
+        this.closest('.has-child').classList.toggle("expand")
+    }
+}
